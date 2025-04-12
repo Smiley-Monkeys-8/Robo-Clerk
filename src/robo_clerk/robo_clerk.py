288b48ddc03118f757d0b7a34ed3774994d9cf2a
Alexandru But <butalexandru@gmail.com>
2025-04-12 10:52:00 +0200

    move decision making to a separate module
    Signed-off-by: Alexandru But <butalexandru@gmail.com>

--- src/robo_clerk/robo_clerk.py
+++ src/robo_clerk/robo_clerk.py
@@ -1,6 +1,7 @@
 import json
 import os
 from dotenv import load_dotenv
+from robo_clerk.decider.judge import Decision, manual_decision
 from robo_clerk.doc_processors.pdf import PDFProcessor
 from robo_clerk.jb_api import JB_send_decision, JB_start_game
 import time
@@ -8,9 +9,9 @@ import time
 # Load the .env file from the project root
 load_dotenv()
 
-def make_decision():
-    decision = input("Choose your action (Accept/Reject): ").strip()
-    return decision
+def make_decision(manual: bool = True) -> Decision:
+    if manual:
+        return manual_decision()
 
 def process_pdf():
     input_folder_path = "downloads"
@@ -35,7 +36,7 @@ def play_game():
         process_documents()
         decision = make_decision()
         time.sleep(1)
-        success = JB_send_decision(api_url, api_key, game_session, decision)
+        success = JB_send_decision(api_url, api_key, game_session, decision.value)
         if success:
             print("✅ Good move! Keep going...\n")
         else:
