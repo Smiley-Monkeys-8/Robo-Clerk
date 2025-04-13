12897aa3eff11f5f9093507afc521360ef494502
margarita2104 <margarita.p.ferreira@gmail.com>
2025-04-13 10:52:18 +0200

    integrate frontend and backend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,15 +4,26 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="bg-blue-zodiac text-white py-4 px-5 border-b border-white flex justify-between items-center">
-      <img src={Logo} alt="Julius Baer Logo" />{" "}
+      <Link to="/">
+        <img src={Logo} alt="Julius Baer Logo" />{" "}
+      </Link>
       <ul className="flex gap-10">
-        <Link to="/" className="text-xl hover:underline-offset-4 hover:underline">
+        <Link
+          to="/"
+          className="text-xl hover:underline-offset-4 hover:underline"
+        >
           Home
         </Link>
-        <Link to="how-it-works" className="text-xl hover:underline-offset-4 hover:underline">
+        <Link
+          to="how-it-works"
+          className="text-xl hover:underline-offset-4 hover:underline"
+        >
           How it works
         </Link>
-        <Link to="/review" className="text-xl hover:underline-offset-4 hover:underline">
+        <Link
+          to="/review"
+          className="text-xl hover:underline-offset-4 hover:underline"
+        >
           Review
         </Link>
       </ul>
